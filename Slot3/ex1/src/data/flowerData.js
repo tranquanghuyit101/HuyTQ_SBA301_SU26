7bd3e4ef44a49f25f3b3e7260209e883ad9844a6
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-05-14 20:35:17 +0700

    Lab 01 - Working with ReactJS application

---
+++ Slot3/ex1/src/data/flowerData.js
@@ -0,0 +1,10 @@
+export const flowerList = [
+    { id: 1, name: "Catus", type: "Thân sinh", image: "/images/cactusflower.jpg" },
+    { id: 2, name: "Hydrangea", type: "Thân thảo", image: "/images/hydrangea.jpg" },
+    { id: 3, name: "Lily", type: "Thân củ", image: "/images/lily.jpg" },
+    { id: 4, name: "Hibiscus", type: "Thân gỗ", image: "/images/hibiscus.jpg" },
+    { id: 5, name: "Sunflower", type: "Thân thảo", image: "/images/sunflower.jpg" },
+    { id: 6, name: "Sakura", type: "Thân gỗ", image: "/images/sakura.jpg" },
+    { id: 7, name: "Peony", type: "Thân thảo", image: "/images/peony.jpg" },
+    { id: 8, name: "Lotus", type: "Thủy sinh", image: "/images/lotus.jpg" },
+  ];
