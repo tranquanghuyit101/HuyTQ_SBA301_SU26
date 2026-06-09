2e043591cac8bf8d54f4c682e2d976508353c884
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-06-09 21:14:49 +0700

    Slot17-18: progress-test-2-tranquanghuyit101

--- Slot3/ex1/src/data/banner.js
+++
@@ -1,5 +0,0 @@
- export const images = [
-    { url: "/images/sakura.jpg", title: "Sakura", desc: "Fragility, ephemeral beauty, and new beginnings" },
-    { url: "/images/lily.jpg", title: "Lily", desc: "Purity, harmony, grace, beauty, and rebirth" },
-    { url: "/images/redroses.jpg", title: "Red Roses", desc: "Intense love, passion, and sacrifice in a relationship." }
-  ];
